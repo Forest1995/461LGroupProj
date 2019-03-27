93bc5012668b9785250d6906361f5ab04a206fe7
jmm9683 <jakemorrissey@utexas.edu>
2019-03-27 17:09:58 -0500

    api call test

--- skiWhere/src/pages/resorts/resorts.ts
+++ skiWhere/src/pages/resorts/resorts.ts
@@ -34,7 +34,25 @@ export class ResortsPage {
   //   //this.api.postResort("3/11/2019","3/22/2019","TX",1).then((res)=>{
   //   //update ui
   //   // })
-  // }
+    // }
+  load() {
+    // don't have the data yet
+    return new Promise(resolve => {
+      // We're using Angular HTTP provider to request the data,
+      // then on the response, it'll map the JSON data to a parsed JS object.
+      // Next, we process the data and resolve the promise with the new data.
+      this.http.get('https://randomuser.me/api/?results=10')
+        .map(res => res.json())
+        .subscribe(data => {
+          // we've got back the raw data, now generate the core schedule data
+          // and save the data for later reference
+          this.data = data;
+          console.log(this.data);
+          resolve(this.data);
+
+        });
+    });
+  }
 
   goToHotelsNearSilverton(){
     console.log(this.data);
