a97e8ed8eabf966eb12277e341887a1a9400c290
jmm9683 <jakemorrissey@utexas.edu>
2019-03-27 17:44:38 -0500

    framework added for get requests

--- skiWhere/src/pages/flights/flights.ts
+++ skiWhere/src/pages/flights/flights.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { ConfirmPage } from '../confirm/confirm';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'page-flights',
@@ -8,7 +10,10 @@ import { ConfirmPage } from '../confirm/confirm';
 })
 export class FlightsPage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private http: HttpClient) {
+  }
+  load(){
+    console.log(this.http.get('https://randomuser.me/api/?results=10'));
   }
   goToConfirm(params){
     if (!params) params = {};
--- skiWhere/src/pages/hotels-near-silverton/hotels-near-silverton.ts
+++ skiWhere/src/pages/hotels-near-silverton/hotels-near-silverton.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { FlightsPage } from '../flights/flights';
 import { ConfirmPage } from '../confirm/confirm';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'page-hotels-near-silverton',
@@ -9,7 +11,10 @@ import { ConfirmPage } from '../confirm/confirm';
 })
 export class HotelsNearSilvertonPage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private http: HttpClient) {
+  }
+  load(){
+    console.log(this.http.get('https://randomuser.me/api/?results=10'));
   }
   goToFlights(params){
     if (!params) params = {};
