e9f81db351e727e3761b2956e3082d6faacadda1
Unknown <Salpathare@utexas.edu>
2019-03-28 19:26:29 -0500

    Enabled CORS

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const rp = require('request-promise');
+var cors = require('cors')
 var cheerio = require('cheerio');
 
 const app = express()
+app.use(cors())
+
 app.use(express.json());
 
 const port = process.env.PORT || 3000
